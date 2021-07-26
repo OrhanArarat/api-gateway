6b95a7f4717978813eb944c7982d5bc3f3b0a6a0
orhanararat <orhanararat01@gmail.com>
2021-07-26 14:01:44 +0300

    Added api-gateway configuration

---
+++ src/main/java/com/orhanararat/config/ApiGatewayConfiguration.java
@@ -0,0 +1,24 @@
+package com.orhanararat.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGatewayConfiguration {
+
+    @Bean
+    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p.path("/get")
+                        .filters(f -> f.addRequestHeader("MyHeader", "MyURI")
+                                .addRequestParameter("Param", "MyValue"))
+                        .uri("http://httpbin:80"))
+                .route(p -> p.path("/currency-exchange/**")
+                        .uri("lb://currency-exchange"))
+
+                .build();
+    }
+
+}
